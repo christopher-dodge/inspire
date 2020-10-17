ccd452b5a728425eb94be0623f67d13e2ab3c334
Christopher Dodge <dodge.christopher@icloud.com>
2020-10-17 17:23:30 -0400

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import QuoteButton from './Components/QuoteButton';
 
 function App() {
+  // const [quote, setQuote] = useState('');
+
+  // Click handler method
+  // const handleButtonClick = function(){
+
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello There, This is the Genie</h1>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-const   express   = require('express'),
-        dotenv    = require('dotenv').config();
-        port      = process.env.PORT || 3000;
-        app       = express();
+// INSPIRE APP 1.0
+const   express         = require('express'),
+        dotenv          = require('dotenv').config(),
+        port            = process.env.PORT || 9001,
+        morgan          = require('morgan'),
+        path            = require('path'),
+        testAPIRouter   = require('./routes/api'),
+        app             = express();
 
-// Routes
+// Tell node where to serve static files from
+app.use(express.static(path.join(__dirname, '../client/public')));
 app.get('/', (req, res) => {
-    res.send("This will be the Splash page!");
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 // Server
