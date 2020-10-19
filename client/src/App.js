81d60165e00c6c5d4cfd5ba0919ac88a94aee9f3
Christopher Dodge <dodge.christopher@icloud.com>
2020-10-19 18:18:23 -0400

    Add click handler for QuoteButton component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,6 @@ import './App.css';
 import QuoteButton from './Components/QuoteButton';
 
 function App() {
-  // const [quote, setQuote] = useState('');
-
-  // Click handler method
-  // const handleButtonClick = function(){
-
-  // }
 
   return (
     <div className="App">
--- client/src/Components/QuoteButton/QuoteButton.jsx
+++ client/src/Components/QuoteButton/QuoteButton.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
+// import { useState } from 'react';
 
 export default function QuoteButton() {
-    return (
-        <div>
-            <Button variant="contained" color="secondary">
-                Genie, Inspire Me
-            </Button>
-        </div>
-    )
+  // const [quote, setQuote] = useState('');
+
+  // Click handler method
+  const handleButtonClick = function(){
+    console.log('The button was clicked!');
+  }
+
+  return (
+    <div>
+      <Button variant="contained" color="secondary" onClick={handleButtonClick}>
+        Genie, Inspire Me
+      </Button>
+      
+    </div>
+  )
 }
