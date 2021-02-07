c6a0771fc5b3a95ff677b77090a2db26c76cab87
Christopher Dodge <dodge.christopher@icloud.com>
2021-02-07 15:34:48 -0500

    Reform server as async

--- server/server.js
+++ server/server.js
@@ -10,32 +10,43 @@ const   express           = require('express'),
         connectToDatabase = require('./database/connection'),
         app               = express();
 
-// Database connection
-connectToDatabase();
+async function startServer() {
+  // Database connection, will be true or false
+  const dbConnected = await connectToDatabase();
+  console.log('\ndbConnected: ' + dbConnected);
 
-// Test insert to db
-// const quote = new Quote({
-//   author: 'Mr. Bigglesworth',
-//   body: "I am a cat, therefore I am."
-// })
+  // Test insert to db
+  // const quote = new Quote({
+  //   author: 'Mr. Bigglesworth',
+  //   body: "I am a cat, therefore I am."
+  // })
 
-// quote.save().then((quote) => console.log("Inserted"));
+  // quote.save().then((quote) => console.log("Inserted"));
 
-// Find all quotes and return total count
-const allQuotes = {};
-Quote.find(allQuotes).then((quotes) => console.log(`Found ${quotes.length} quotes in the database`));
+  // Find all quotes and return total count
+  const allQuotes = {};
+  Quote.find(allQuotes).then((quotes) => console.log(`Found ${quotes.length} quotes in the database`));
 
-// Tell node where to serve static files from
-app.use(express.static(path.join(__dirname, '../client/public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
+  // Tell node where to serve static files from
+  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  });
 
-app.use('/api', api);
+  app.use('/api', api);
 
-// Server
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+  // If db is not connected, server will not listen
+  let server;
+  if (dbConnected) {
+    server = app.listen(port).on('listening', () => {
+      console.log(`🌎  ==> API Server now listening on PORT ${port}!`);
+    });
+  } else {
+    console.log('No db connection!');
+  }
+  return server;
+}
+
+const server = startServer();
