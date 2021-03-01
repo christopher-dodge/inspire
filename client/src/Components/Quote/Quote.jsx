a9e9a9e10d787466aa0c01a80e343d718f81c4ee
Christopher Dodge <dodge.christopher@icloud.com>
2021-03-01 17:34:59 -0500

    Remove spacebar function

--- client/src/Components/Quote/Quote.jsx
+++ client/src/Components/Quote/Quote.jsx
@@ -3,17 +3,8 @@ import React from 'react'
 export default function Quote(props) {
   console.log(props);
 
-  document.body.onkeyup = function(e){
-    if(e.keyCode === 32){
-        // console.log('spacebar was pressed!');
-        props.getRandomQuote();
-    }
-  }
-
   return (
-    // <div onClick={props.getRandomQuote}>
-    <div>
-      
+    <div onClick={props.getRandomQuote}>
       <div className="center">
         <h2>{props.quote.Quote}</h2>
         <h5>{props.quote.Author}</h5>
